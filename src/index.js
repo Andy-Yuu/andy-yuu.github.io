b5025dc52538f403b8c8acf2cca90362b6ebdee3
Andy Yu <yuandy1031@gmail.com>
2021-10-16 21:41:01 -0400

    use classnames to override mui styling

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import { theme } from './theme';
 import { ThemeProvider } from '@mui/styles';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
