edaceab6807c2bab38d01dd808d9e026f0f77d01
Andy Yu <yuandy1031@gmail.com>
2021-10-16 21:42:05 -0400

    working on people also search for section

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -5,22 +5,30 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import pic from '../images/pic.jpg';
 
 const useStyles = makeStyles(theme => ({
+	card: {
+		[theme.breakpoints.down('md')]: {
+			display: 'flex', 
+			justifyContent:'center',
+		},
+		[theme.breakpoints.up('lg')]: {
+			marginLeft: '60px',
+		},
+	},
 	name: {
-		fontWeight: 600,
 		padding: '16px',
 	},
 	profileIcons: {
-		margin: '3px', 
-		width: '60px', 
+		marginTop: '4px', 
+		width: '72px', 
 		height: '60px', 
 		textAlign: 'center'
-	}
+	},
 }))
 
 function AboutCard(props) {
 	const classes = useStyles();
   return (
-		<div>
+		<div className={classes.card}>
 			<Card sx={{ maxWidth: 336 }}>
 				<CardMedia 
 					component='img'
@@ -48,6 +56,57 @@ function AboutCard(props) {
 							<Link href='https://www.linkedin.com/in/andy-yuu/' underline='hover'>
 								<LinkedInIcon style={{color: '#0072b1'}}/>
 								<Typography variant='body2'>LinkedIn</Typography>
+							</Link>
+						</Box>
+					</Grid>
+					<Typography variant='subtitle1'>People also search for</Typography>
+					<Grid container>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Sunny</Typography>
+							</Link>
+						</Box>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Jimmy</Typography>
+							</Link>
+						</Box>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Steven</Typography>
+							</Link>
+						</Box>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Richie</Typography>
+							</Link>
+						</Box>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Michael</Typography>
+							</Link>
+						</Box>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Eddie</Typography>
+							</Link>
+						</Box>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Simon</Typography>
+							</Link>
+						</Box>
+						<Box className={classes.profileIcons}>
+							<Link href='' underline='hover'>
+								<GitHubIcon style={{color: 'black'}}/>
+								<Typography variant='body2'>Benny</Typography>
 							</Link>
 						</Box>
 					</Grid>
